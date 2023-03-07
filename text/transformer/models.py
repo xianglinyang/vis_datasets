4d7af67233742b269d9c0e6b0296fae59020f045
Yang Xianglin <yxlyzxl@gmail.com>
2023-03-07 10:59:27 +0800

    xlnet model

--- text/transformer/models.py
+++ text/transformer/models.py
@@ -66,3 +66,51 @@ class BERTGRUSentiment(nn.Module):
         output = self.out(hidden)
         #output = [batch size, out dim]
         return output
+
+class XLNETGRU(nn.Module):
+    def __init__(self,
+                 xlnet,
+                 hidden_dim,
+                 output_dim,
+                 n_layers,
+                 bidirectional,
+                 dropout):
+        
+        super().__init__()
+        self.xlnet = xlnet
+        embedding_dim = self.xlnet.config.to_dict()['d_model']
+        self.rnn = nn.GRU(embedding_dim,
+                          hidden_dim,
+                          num_layers = n_layers,
+                          bidirectional = bidirectional,
+                          batch_first = True,
+                          dropout = 0 if n_layers < 2 else dropout)
+        
+        self.out = nn.Linear(hidden_dim * 2 if bidirectional else hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, **text):
+        with torch.no_grad():
+            embedded = self.xlnet(**text)[0]
+        _, hidden = self.rnn(embedded)
+        if self.rnn.bidirectional:
+            hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1))
+        else:
+            hidden = self.dropout(hidden[-1,:,:])
+        output = self.out(hidden)
+        return output
+    
+    def feature(self,**text):
+        with torch.no_grad():
+            embedded = self.xlnet(**text).hidden_states
+        _, hidden = self.rnn(embedded)
+        if self.rnn.bidirectional:
+            hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1))
+        else:
+            hidden = self.dropout(hidden[-1,:,:])
+        return hidden
+    
+    def prediction(self, hidden):
+        output = self.out(hidden)
+        #output = [batch size, out dim]
+        return output
